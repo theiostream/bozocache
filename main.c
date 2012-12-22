5f3a98eccf793075db66bf4bc66c6cdbbab789af
Daniel Ferreira (theiostream) <daniel.calibeta@gmail.com>
2012-12-22 01:13:50 -0200

    Add License (WTFPLv3)

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/* 
+This project is licensed under the terms of the
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE, version 3,
+as published by theiostream on March 2012, as it follows:
+
+0. You just DO WHAT THE FUCK YOU WANT TO.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
